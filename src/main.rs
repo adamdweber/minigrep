9b73e50692f3b7ac561ff56cbcb1b6ef3936a57b
adam <adamdweber@gmail.com>
2025-03-26 18:56:42 -0400

    moved errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("searching for {}", config.query);
-    println!(" in path {}", config.file_path);
+    //println!("searching for {}", config.query);
+    //println!(" in path {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("Application Error:  {e}");
